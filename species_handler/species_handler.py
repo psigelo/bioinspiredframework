c1ff1e100f7cbb394c16d39696cd213fe85069e6
Pascal Sigel <pascal.sigel@gmail.com>
2022-07-08 23:08:35 -0400

    species handler and task handler first kind of connectio

---
+++ species_handler/species_handler.py
@@ -0,0 +1,31 @@
+import argparse
+from flask import Flask
+import requests
+
+
+def create_species_handler_app(task_handler_port, task_handler_ip, port):
+    app = Flask(__name__,  instance_relative_config=True)
+
+    url = f"http://{task_handler_ip}:{task_handler_port}/species_handler_register"
+    message = {
+        'specie_handler_port': port,
+        'specie_handler_ip': "127.0.0.1",  # TODO: get it from script
+    }
+    print(f"url:{url}")
+    x = requests.post(url, json=message)
+    print(f"CONNECT RESPONSE {x.text}")
+
+    @app.route('/')
+    def hello():
+        return 'Hello, World!'
+    return app
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port", help="port for species_handler app", default=7000, type=int)
+    parser.add_argument("--task_handler_port", help="port for species_handler app", default=8000, type=int)
+    parser.add_argument("--task_handler_ip", help="port for species_handler app", default="127.0.0.1", type=str)
+    cmd_arguments = parser.parse_args()
+    app_ = create_species_handler_app(cmd_arguments.task_handler_port, cmd_arguments.task_handler_ip, cmd_arguments.port)
+    app_.run(port=cmd_arguments.port)
--- task_handler/task_handler.py
+++ task_handler/task_handler.py
@@ -1,16 +1,19 @@
 import argparse
-from flask import Flask
+from flask import Flask, request
 
 
-def create_app():
-    # create and configure the app
+SPECIES_TASK_LIST = []
+
+
+def create_task_handler_app():
     app = Flask(__name__,  instance_relative_config=True)
 
-    # a simple page that says hello
-    @app.route('/')
-    def hello():
+    @app.route('/species_handler_register', methods=('POST',))
+    def species_handler_register():
+        print(f"{request.json['specie_handler_port']}")
+        SPECIES_TASK_LIST.append({"specie_handler_port": request.json['specie_handler_port'],
+                                  "specie_handler_ip": request.json['specie_handler_ip']})
         return 'Hello, World!'
-
     return app
 
 
@@ -18,5 +21,5 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", help="port for task_handler app", default=8000, type=int)
     cmd_arguments = parser.parse_args()
-    app = create_app()
+    app = create_task_handler_app()
     app.run(port=cmd_arguments.port)
