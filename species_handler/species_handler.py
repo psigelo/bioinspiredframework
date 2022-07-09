710c050fb228902b596daa74ca8a431ca3b9d733
Pascal Sigel <pascal.sigel@gmail.com>
2022-07-09 15:31:51 -0400

    adding version control between connections

--- species_handler/species_handler.py
+++ species_handler/species_handler.py
@@ -1,19 +1,28 @@
 import argparse
+import json
+
 from flask import Flask
 import requests
+import git
 
 
 def create_species_handler_app(task_handler_port, task_handler_ip, port):
+    global git_sha
     app = Flask(__name__,  instance_relative_config=True)
 
     url = f"http://{task_handler_ip}:{task_handler_port}/species_handler_register"
     message = {
         'specie_handler_port': port,
         'specie_handler_ip': "127.0.0.1",  # TODO: get it from script
-
+        "git_sha": git_sha
     }
     print(f"url:{url}")
     x = requests.post(url, json=message)
+    response_post = json.loads(x.text)
+    if response_post["status"] != "correct":
+        print(x.text)
+        exit(-1)
+
     print(f"CONNECT RESPONSE {x.text}")
 
     @app.route('/')
@@ -28,5 +37,9 @@ if __name__ == "__main__":
     parser.add_argument("--task_handler_port", help="port for species_handler app", default=8000, type=int)
     parser.add_argument("--task_handler_ip", help="port for species_handler app", default="127.0.0.1", type=str)
     cmd_arguments = parser.parse_args()
+
+    repo = git.Repo(search_parent_directories=True)
+    git_sha = repo.head.object.hexsha
+
     app_ = create_species_handler_app(cmd_arguments.task_handler_port, cmd_arguments.task_handler_ip, cmd_arguments.port)
     app_.run(port=cmd_arguments.port)
--- task_handler/task_handler.py
+++ task_handler/task_handler.py
@@ -1,6 +1,7 @@
 import argparse
 from flask import Flask, request
-
+import json
+import git
 
 SPECIES_TASK_LIST = []
 
@@ -10,17 +11,24 @@ def create_task_handler_app():
 
     @app.route('/species_handler_register', methods=('POST',))
     def species_handler_register():
+        global git_sha
+        # The following lines are to check that all connections are from the same git commit to not have any version problem
+        if request.json['git_sha'] != git_sha:
+            return json.dumps({"status": "error", "error_message": "git_sha_commit_is_not_the_same"})
         print(f"{request.json['specie_handler_port']}")
         SPECIES_TASK_LIST.append({"specie_handler_port": request.json['specie_handler_port'],
                                   "specie_handler_ip": request.json['specie_handler_ip']})
         print(f"list of species tasks: {SPECIES_TASK_LIST}")
-        return 'Hello, World!'
+        return json.dumps({"status": "correct"})
     return app
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", help="port for task_handler app", default=8000, type=int)
+    repo = git.Repo(search_parent_directories=True)
+    git_sha = repo.head.object.hexsha
+
     cmd_arguments = parser.parse_args()
     app = create_task_handler_app()
     app.run(port=cmd_arguments.port)
